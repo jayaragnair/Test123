41863dd07b792bcf657602ac424330c19948b161
2020wa86535@wilp.bits-pilani.ac.in <jayarag@gmail.com>
2024-04-27 13:09:45 +0530

    adding new function

--- file_1.c
+++ file_1.c
@@ -15,7 +15,7 @@ int main() {
 //function added in branch1
 int func_add(int a, int b)
 {
-	// i am in main branch
+	// i am in branch1
 	// i am making this changes in 
 	// one more change 
 	return a+b;
