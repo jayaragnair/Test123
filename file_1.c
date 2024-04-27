43db01efa05febf6a7b0a8c23633df46988d7bd8
2020wa86535@wilp.bits-pilani.ac.in <jayarag@gmail.com>
2024-04-27 13:21:29 +0530

    Same changes in main

--- file_1.c
+++ file_1.c
@@ -12,11 +12,10 @@ int main() {
     printf("%d + %d = %d", number1, number2, sum);
     return 0;
 }
-//function added in branch1
+
 int func_add(int a, int b)
 {
-	// i am in branch1
-	// i am making this changes in 
-	// one more change 
+		// i am making this changes in 
+
 	return a+b;
 }
